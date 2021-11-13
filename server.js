53d185c080a7b0de26e12183a87c322d6a187434
Alok650 <alokp650@gmail.com>
2021-11-13 10:59:19 +0530

    Basic template

--- server.js
+++ server.js
@@ -5,16 +5,13 @@ var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
 // index page 
 app.get('/', function(req, res) {
     var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+        { name: 'Alok Prasad', organization: "Hackstreet warriors", birth_year: 2001},
+        { name: 'Keshav Gautam', organization: "Hackstreet warriors", birth_year: 2001}
     ];
-    var tagline = "No programming concept is complete without a cute animal mascot.";
+    var tagline = "Car pooling application";
 
     res.render('pages/index', {
         mascots: mascots,
